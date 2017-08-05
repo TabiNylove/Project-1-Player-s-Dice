26b29aa2a30a8ee66d30fb9ed539b3fd9a34c4d5
unknown <ratlarva@gmail.com>
2017-08-05 10:41:04 -0600

    tastedive js

--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,12 @@
-
+var search = "";
+var apiKey = "279156-PlayersD-B5WE9ZBL";
  //"279156-PlayersD-TA0FFDNE";
-var apiKey = "279156-PlayersD-B5WE9ZBL"; 
-var queryURL = "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction&k=" + apiKey;
+ $(".button").on('click', function(event){
+ 	console.log("hello");
+ 	event.preventDefault();
+	search = $("#userInput").val().trim();
+	console.log(search);
+	var queryURL = "https://tastedive.com/api/similar?q=" + search  + "&k=" + apiKey;
 
 console.log(queryURL);
 	$.ajax({
@@ -10,17 +15,25 @@ console.log(queryURL);
       method: "GET"
     }).done(function(response) {
     	console.log(response);
+    	var randResult = Math.floor(Math.random()*20);
+    	console.log(randResult);
+    	var pickedThing = response.Similar.Results[randResult].Name;
+    	console.log(pickedThing);
     })
+})
+
+ 
 
 
 
 
-//     var youTubeApiKey = "AIzaSyD5FZHeHpWCyo0-34E15x9TEmjf2smoFiU"; 
-// var youTubeQueryURL = "https://tastedive.com/api/similar?q=red+hot+chili+peppers%2C+pulp+fiction=" + youTubeApiKey;
-// console.log(youTubeQueryURL);
-// 	$.ajax({
-//       url: youTubeQueryURL,
-//       method: "GET"
-//     }).done(function(response) {
-//     	console.log(response);
-//     })
+
+    var youTubeApiKey = "AIzaSyD5FZHeHpWCyo0-34E15x9TEmjf2smoFiU"; 
+var youTubeQueryURL = "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=" + youTubeApiKey + "&part=snippet,contentDetails,statistics,status"
+console.log(youTubeQueryURL);
+	$.ajax({
+      url: youTubeQueryURL,
+      method: "GET"
+    }).done(function(response) {
+    	console.log(response);
+    })
